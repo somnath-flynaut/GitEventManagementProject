4146b231afac299e808ae4c57a59b820da90cc9f
Shubhamkorde99 <ssk.mkorde24@gmail.com>
2025-04-08 16:13:15 +0530

    LWC(eventRegistrationFrom): Reflection of "Ticket Price" dynamically in runtime #156 added by Group C

--- force-app/main/default/lwc/eventRegistrationForm/eventRegistrationForm.js
+++ force-app/main/default/lwc/eventRegistrationForm/eventRegistrationForm.js
@@ -4,75 +4,91 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ticketprice from '@salesforce/apex/EventController.ticketprice';
 
 export default class EventRegistrationForm extends LightningElement {
-    @track eventName = '';
-    @track eventId = '';
-    @track isLoading = true;
-    @track showPaymentModal = false; // Controls Payment Popup
-    EvtId;
-    ticketType;
-    numTickets;
-    showprice;
-   
-    handleEvt(event)
-    {
-        this.EvtId = event.target.value;
-         this.calculatePrice();
-    }
-    handleTicketTypeChange(event)
-    {
-        this.ticketType = event.target.value;
-        this.calculatePrice();
-    }
-    handlenumber(event)
-    {
-        this.numTickets = event.target.value;
-        this.calculatePrice();
-    }
+@track eventName = '';
+@track eventId = '';
+@track isLoading = true;
+@track showPaymentModal = false; // Controls Payment Popup
+EvtId;
+ticketType;
+numTickets;
+showprice;
+ticketCount ;
 
-    calculatePrice() {
-        if(this.EvtId && this.ticketType && this.numTickets) {
-         ticketprice({ EvtId:this.EvtId, ticketType : this.ticketType, numTickets: this.numTickets })
-         .then(data => {
-             this.showprice=data;
+handleEvt(event)
+{
+    this.EvtId = event.target.value;
+    this.fetchBasePrice(); // fletch base price
+}
+handleTicketTypeChange(event)
+{
+    this.ticketType = event.target.value;
+    this.calculatePrice(); // for multiplyer
+}
+handlenumber(event)
+{
+    const count = parseInt(event.target.value, 10);
+    this.ticketCount = count;
+    this.calculatePrice();
+}
+
+fetchBasePrice() {
+    if(this.EvtId) {
+        ticketprice({EvtId : this.EvtId})
+        .then((data) => {
+            this.price = data;
         })
-        .catch((error) => {
-         console.error('Error fetching price:', error);
-         this.showprice = 0;
-     });
-        }
-     }
+        .catch((error)=> {
+            console.error('Error fetching base price:', error);
+                this.showprice = 0;
+        })
+    }
+}
 
-
-
-
-
-    @wire(CurrentPageReference)
-    getPageReference(currentPageReference) {
-        if (currentPageReference) {
-            this.eventName = currentPageReference.state.eventName || 'Unknown Event';
-            this.eventId = currentPageReference.state.eventId || '';
-        }
-        this.isLoading = false;
+calculatePrice() {
+    if(this.price && this.ticketType && this.ticketCount)
+    {
+    let finalPrice = this.price;
+    if (this.ticketType === 'VIP') {
+        finalPrice *= 1.5 *this.ticketCount;
+    }  if (this.ticketType === 'Student') {
+        finalPrice *= 0.85 *this.ticketCount ;
+    }
+    else if (this.ticketType === 'General') {
+        finalPrice *= 1 *this.ticketCount ;
+    }
+    
+       
+    this.showprice = finalPrice;
+    }
     }
 
-    // Open Payment Popup
-    handleProceedToPay() {
-        this.showPaymentModal = true;
+@wire(CurrentPageReference)
+getPageReference(currentPageReference) {
+    if (currentPageReference) {
+        this.eventName = currentPageReference.state.eventName || 'Unknown Event';
+        this.eventId = currentPageReference.state.eventId || '';
     }
+    this.isLoading = false;
+}
 
-    // Close Payment Popup
-    closePaymentModal() {
-        this.showPaymentModal = false;
-    }
+// Open Payment Popup
+handleProceedToPay() {
+    this.showPaymentModal = true;
+}
 
-    // Show success message when record is saved
-    handleSuccess(event) {
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title: 'Success',
-                message: 'Registration Successful! Record ID: ' + event.detail.id,
-                variant: 'success',
-            })
-        );
-    }
+// Close Payment Popup
+closePaymentModal() {
+    this.showPaymentModal = false;
+}
+
+// Show success message when record is saved
+handleSuccess(event) {
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Success',
+            message: 'Registration Successful! Record ID: ' + event.detail.id,
+            variant: 'success',
+        })
+    );
+}
 }
